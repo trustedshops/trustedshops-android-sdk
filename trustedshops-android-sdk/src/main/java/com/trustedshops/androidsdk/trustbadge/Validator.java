ae4139bd3e30620392f31213c208ea74c9d4071a
Gero Herkenrath <Gero.Herkenrath@trustedshops.de>
2016-11-24 14:22:00 +0100

    changed the email validation so it allows missing emails added test-classic.de tsid to example project (I have that on iOS, too, so I wanna compare behavior between them).

--- trustedshops-android-sdk/src/main/java/com/trustedshops/androidsdk/trustbadge/Validator.java
+++ trustedshops-android-sdk/src/main/java/com/trustedshops/androidsdk/trustbadge/Validator.java
@@ -20,6 +20,7 @@ public class Validator {
     }
 
     public static boolean isValidEmailAddress(String email) {
+        if (email == null || email == "") {return true;} // allow missing email
         String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
         java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
         java.util.regex.Matcher m = p.matcher(email);
