9153dbcd77330cfc406b804be4672113e174260a
Gero Herkenrath <Gero.Herkenrath@trustedshops.de>
2017-02-06 17:35:48 +0100

    far from done, reorganized code a bit

--- trustedshops-android-sdk/src/main/java/com/trustedshops/androidsdk/trustbadge/Trustbadge.java
+++ trustedshops-android-sdk/src/main/java/com/trustedshops/androidsdk/trustbadge/Trustbadge.java
@@ -8,12 +8,15 @@ import android.net.Uri;
 import android.os.Handler;
 import android.os.Message;
 import android.text.Html;
+import android.util.LayoutDirection;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -34,8 +37,6 @@ import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-
-import static android.R.attr.width;
 
 
 public class Trustbadge {
@@ -371,20 +372,7 @@ public class Trustbadge {
             _alreadyInjected = false;
             _webView = new WebView(_activity);
             _webView.clearCache(true);
-            _webView.setWebChromeClient(new WebChromeClient(){
-                public void onProgressChanged(WebView view, int newProgress) {
-                    if (newProgress == 100 && !_alreadyInjected) {
 
-                        // TODO: finish js injection and such
-                        view.loadUrl("javascript:injectTrustbadge('"+ getTsId() +"', '"+ getTrustcardEndpoint() +"')");
-
-                        if (isLoggingActive()) {
-                            Log.d("TSDEBUG","Page loaded");
-                        }
-                        _alreadyInjected = true;
-                    }
-                }
-            });
 
             _webView.getSettings().setJavaScriptEnabled(true);
             _webView.getSettings().setDisplayZoomControls(false);
@@ -400,6 +388,10 @@ public class Trustbadge {
             if (isDebugActive()) {
                 _webView.setWebContentsDebuggingEnabled(true);
             }
+
+//            _webView.setLayoutParams(new ViewGroup.LayoutParams(600, 400));
+//            _webView.setForegroundGravity(Gravity.BOTTOM);
+//            _webView.setBottom(0);
 
             final MaterialDialog dialog = new MaterialDialog.Builder(_activity)
                     .customView(_webView, false)
@@ -429,15 +421,20 @@ public class Trustbadge {
                             if ((oldWidth != width || oldHeight != height) && width != 0 && height != 0) {
                                 int offsetW = 0;
                                 int offsetH = 0;
+                                int height2 = height;
+                                if (height2 > 900) { height2 = 900; }
                                 View containingView = dialog.getWindow().getDecorView();
                                 offsetW = containingView.getPaddingLeft() + containingView.getPaddingRight();
                                 offsetH = containingView.getPaddingTop() + containingView.getPaddingBottom();
-                                dialog.getWindow().setLayout(width + offsetW, height + offsetH);
+                                dialog.getWindow().setLayout(width + offsetW, height2 + offsetH);
                                 ViewGroup.LayoutParams params = _webView.getLayoutParams();
                                 params.width = width;
-                                params.height = height;
+                                params.height = height2;
                                 _webView.setLayoutParams(params);
                                 _webView.forceLayout();
+                                if (!dialog.isShowing()) {
+                                    dialog.show();
+                                }
                             }
                         }
                     });
@@ -447,7 +444,27 @@ public class Trustbadge {
             JsInterface jsInterface = new JsInterface(dialog, resizeCallback);
             _webView.addJavascriptInterface(jsInterface, "jsInterface");
             _webView.loadUrl("file:///android_asset/trustcard_page.html");
-            dialog.show();
+            _webView.setWebChromeClient(new WebChromeClient(){
+                public void onProgressChanged(WebView view, int newProgress) {
+                    if (newProgress == 100 && !_alreadyInjected) {
+
+                        // TODO: finish js injection and such
+                        view.loadUrl("javascript:injectTrustbadge('"+ getTsId() +"', '"+ getTrustcardEndpoint() +"')");
+
+                        if (isLoggingActive()) {
+                            Log.d("TSDEBUG","Page loaded");
+                        }
+                        _alreadyInjected = true;
+
+//                        dialog.show();
+                        Log.d("GERODEV", "Would show now!");
+                    }
+                }
+            });
+//            dialog.getWindow().setLayout(996, 1317);
+            dialog.getWindow().getAttributes().windowAnimations = R.style.DialogTheme;
+//            dialog.getCustomView().getRootView().setBackgroundColor(Color.parseColor("#FFDC0F"));
+//            dialog.show();
         }
     };
 
